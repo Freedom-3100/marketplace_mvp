d575ceb2287ca1fa578dc4c20071718d3accde50
aqumu <aqumu.vm@gmail.com>
2025-10-04 18:43:03 +0700

    define typography and colorscheme

--- app/src/main/java/com/example/marketplace_mvp/ui/theme/Typography.kt
+++ app/src/main/java/com/example/marketplace_mvp/ui/theme/Typography.kt
@@ -1,2 +1,37 @@
 package com.example.marketplace_mvp.ui.theme
 
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.marketplace_mvp.R
+
+
+val AppFont = FontFamily(
+    Font(R.font.wixmadefortext_regular, weight = FontWeight.Normal),
+    Font(R.font.wixmadefortext_medium, weight = FontWeight.Medium),
+    Font(R.font.wixmadefortext_bold, weight = FontWeight.Bold),
+    Font(R.font.wixmadefortext_extrabold, weight = FontWeight.ExtraBold)
+)
+// Custom font (if you have it in res/font)
+
+val AppTypography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = AppFont,
+        fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = AppFont,
+        fontSize = 20.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = AppFont,
+        fontSize = 12.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = AppFont,
+        fontSize = 12.sp
+    ),
+)
